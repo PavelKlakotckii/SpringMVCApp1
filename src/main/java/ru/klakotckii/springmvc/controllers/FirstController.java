08aa9573ee0efd3de5a2649db645e7ad3a62ba6d
pavelklak <qaz789wsx>
2021-03-18 15:18:43 +0300

    отображение переданных параметров на экране и калькулятор

--- src/main/java/ru/klakotckii/springmvc/controllers/FirstController.java
+++ src/main/java/ru/klakotckii/springmvc/controllers/FirstController.java
@@ -1,20 +1,54 @@
 package ru.klakotckii.springmvc.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(){
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
+        model.addAttribute("message", "Hello, " + name + " " + surname + "!");
         return "first/hello";
     }
 
-    @GetMapping("goodbye")
+    @GetMapping("/goodbye")
     public String goodbyePage() {
         return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam(value = "a", required = false) int a,
+                             @RequestParam(value = "b", required = false) int b,
+                             @RequestParam(value = "action", required = false) String action,
+                             Model model) {
+        double result;
+        switch (action) {
+            case "multiplication":
+                result = a * b;
+                break;
+            case "addition":
+                result = a + b;
+                break;
+            case "subtraction":
+                result = a - b;
+                break;
+            case "division":
+                result = a / (double)b;
+                break;
+            default:
+                result = 0;
+                break;
+        }
+        model.addAttribute("result", "Result: " + result);
+        return "first/calculator";
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -2,5 +2,6 @@
 <body>
 <h2>Hello World!</h2>
 <a href="/first/hello">Hello</a> or <a href="/first/goodbye">Goodbye</a>
+or <a href="/first/calculator">Calculator</a>
 </body>
 </html>
